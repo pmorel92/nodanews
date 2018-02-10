822939c1495992b7b2f0b8556bc2fae449965755
pmorel92 <philippe.a.b.morel@gmail.com>
2018-02-10 06:15:43 +0000

    cjange

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
