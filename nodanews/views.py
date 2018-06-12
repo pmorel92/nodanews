92613964e54698d479643f7a8135d8d36fb7f251
pmorel92 <philippe.a.b.morel@gmail.com>
2018-06-12 21:10:50 +0000

    ok

--- nodanews/views.py
+++ nodanews/views.py
@@ -15,12 +15,6 @@ def get_item(dictionary, key):
 
 
 def helen(request):
-	node1 = Node.objects.random()
-	node2 = Node.objects.random()
-	node3 = Node.objects.random()
-	node4 = Node.objects.random()
-	node5 = Node.objects.random()
-	node6 = Node.objects.random()
 	issues = PoliticalIssue.objects.all()[0:3]
 	opeds = Breaking_Link.objects.filter( region__id=8 )
 	breaking_links = Breaking_Link.objects.all().exclude( region__id=8 )
@@ -30,10 +24,11 @@ def helen(request):
 	videos = LiveVideo.objects.filter( region__id=8 )
 	indepths = Analysis.objects.all()[0:4]
 	blogs = Blog.objects.all()[0:5]
+	recents = Node.objects.all()[1:3].order_by('-posted')
 	issue_links = {
         n: PoliticalBiasNews.objects.filter(issue__id = n.id).order_by('-posted') for n in issues
     }
-	return render(request, 'nodanews/index.html', {'node1': node1, 'node2': node2, 'node3': node3,'node4': node4,'node5': node5,'node6': node6, 'issues': issues, 'issue_links': issue_links, 'opeds': opeds, 'conservaturds': conservaturds, 'videos': videos, 'headlines': headlines, 'libtards': libtards, 'breaking_links': breaking_links, 'blogs': blogs, 'indepths': indepths})
+	return render(request, 'nodanews/index.html', {'recents': recents, 'issues': issues, 'issue_links': issue_links, 'opeds': opeds, 'conservaturds': conservaturds, 'videos': videos, 'headlines': headlines, 'libtards': libtards, 'breaking_links': breaking_links, 'blogs': blogs, 'indepths': indepths})
 
 def cassandra(request):
     opeds = Node.objects.all()[3:8]
