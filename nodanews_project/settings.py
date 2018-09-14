f8cad8252715ed7710706a0c3faf9f05dde77a0b
pmorel92 <philippe.a.b.morel@gmail.com>
2018-09-14 17:43:28 +0000

    middleware

--- nodanews_project/settings.py
+++ nodanews_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
